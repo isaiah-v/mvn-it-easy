afa5ab8a839555a53aeb999d95535d264f200c96
isaiah.v <isaiah.v@comcast.net>
2024-08-08 01:08:35 -0700

    Added delete for basic-auth users

--- src/main/kotlin/org/ivcode/mvn/controllers/BasicAuthController.kt
+++ src/main/kotlin/org/ivcode/mvn/controllers/BasicAuthController.kt
@@ -3,10 +3,12 @@ package org.ivcode.mvn.controllers
 import org.ivcode.mvn.services.basicauth.BasicAuthService
 import org.ivcode.mvn.services.basicauth.model.BasicAuthUser
 import org.ivcode.mvn.services.basicauth.model.CreateBasicAuthUser
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 /**
@@ -31,5 +33,10 @@ public class BasicAuthController(
             createBasicAuthUser.password,
             createBasicAuthUser.write
         )
+    }
+
+    @DeleteMapping
+    public fun deleteUser(@RequestParam username: String) {
+        basicAuthService.deleteUser(username)
     }
 }
